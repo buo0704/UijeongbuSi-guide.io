51351cb60178bee6ef7a56f01e407927d5d1e6da
이이섭 <aa990724@naver.com>
2025-05-20 17:00:21 +0900

    의정부시 지도 불러오기

--- js/uijeongbusi-map.js
+++ js/uijeongbusi-map.js
@@ -1,5 +1,5 @@
 // Ajax로 Uijeongbu-City-guide.html에서 Uijeongbu-City-Map.html파일을 가져옴
-fetch('uijeongbu-City-Map.html')
+fetch('uijeongbusi-map.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('map-container').innerHTML = data;
