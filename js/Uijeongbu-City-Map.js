50da325fb3d9241a2288398b187435d30a6eef99
이이섭 <aa990724@naver.com>
2025-05-20 11:48:08 +0900

    의정부시 지도를 가져오는 자바스크립트

--- js/Uijeongbu-City-Map.js
+++ js/Uijeongbu-City-Map.js
@@ -1,5 +1,5 @@
 // Ajax로 Uijeongbu-City-guide.html에서 Uijeongbu-City-Map.html파일을 가져옴
-fetch('Uijeongbu-City-Map.html')
+fetch('uijeongbu-City-Map.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('map-container').innerHTML = data;
